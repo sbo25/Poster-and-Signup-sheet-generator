f39ee9d4070b147ab794d5ffe906ec1e3127f9a8
sbo25 <sbo25@cornell.edu>
2016-07-16 13:57:20 -0400

    Initial Commit
    Contains complete code for poster and partial code for sign up sheet

---
+++ EventGenerator/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿using System;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.what = new System.Windows.Forms.TextBox();
+            this.who = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.where = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.waitlist = new System.Windows.Forms.Label();
+            this.waitlistcount = new System.Windows.Forms.NumericUpDown();
+            this.attendee = new System.Windows.Forms.Label();
+            this.attendeecount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.waitlistcount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attendeecount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Poster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // what
+            // 
+            this.what.Location = new System.Drawing.Point(84, 38);
+            this.what.Name = "what";
+            this.what.Size = new System.Drawing.Size(200, 20);
+            this.what.TabIndex = 1;
+            this.what.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // who
+            // 
+            this.who.Location = new System.Drawing.Point(84, 64);
+            this.who.Name = "who";
+            this.who.Size = new System.Drawing.Size(200, 20);
+            this.who.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Event Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Host(s)";
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(84, 119);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(200, 20);
+            this.date.TabIndex = 5;
+            // 
+            // where
+            // 
+            this.where.Location = new System.Drawing.Point(84, 93);
+            this.where.Name = "where";
+            this.where.Size = new System.Drawing.Size(200, 20);
+            this.where.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Where";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Date";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(84, 143);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(200, 20);
+            this.time.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Time";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 25);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Select Picture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Size";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(84, 170);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "Half";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(105, 229);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Type";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(105, 6);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 17;
+            this.comboBox2.Text = "Poster";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // waitlist
+            // 
+            this.waitlist.AutoSize = true;
+            this.waitlist.Location = new System.Drawing.Point(136, 173);
+            this.waitlist.Name = "waitlist";
+            this.waitlist.Size = new System.Drawing.Size(51, 13);
+            this.waitlist.TabIndex = 18;
+            this.waitlist.Text = "Waitlist #";
+            // 
+            // waitlistcount
+            // 
+            this.waitlistcount.Location = new System.Drawing.Point(193, 171);
+            this.waitlistcount.Name = "waitlistcount";
+            this.waitlistcount.Size = new System.Drawing.Size(36, 20);
+            this.waitlistcount.TabIndex = 20;
+            this.waitlistcount.ValueChanged += new System.EventHandler(this.waitlistcount_ValueChanged);
+            // 
+            // attendee
+            // 
+            this.attendee.AutoSize = true;
+            this.attendee.Location = new System.Drawing.Point(12, 173);
+            this.attendee.Name = "attendee";
+            this.attendee.Size = new System.Drawing.Size(60, 13);
+            this.attendee.TabIndex = 21;
+            this.attendee.Text = "Attendee #";
+            // 
+            // attendeecount
+            // 
+            this.attendeecount.Location = new System.Drawing.Point(84, 171);
+            this.attendeecount.Name = "attendeecount";
+            this.attendeecount.Size = new System.Drawing.Size(36, 20);
+            this.attendeecount.TabIndex = 22;
+            this.attendeecount.ValueChanged += new System.EventHandler(this.attendeecount_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 295);
+            this.Controls.Add(this.attendeecount);
+            this.Controls.Add(this.attendee);
+            this.Controls.Add(this.waitlistcount);
+            this.Controls.Add(this.waitlist);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.where);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.who);
+            this.Controls.Add(this.what);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Poster Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.waitlistcount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attendeecount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox what;
+        private System.Windows.Forms.TextBox who;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.TextBox where;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label waitlist;
+        private System.Windows.Forms.NumericUpDown waitlistcount;
+        private System.Windows.Forms.Label attendee;
+        private System.Windows.Forms.NumericUpDown attendeecount;
+    }
+}
+
---
+++ EventGenerator/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private string path;
+        private string size = "Half";
+        private string type = "Poster";
+        private int attnum;
+        private int waitnum;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Half");
+            comboBox1.Items.Add("Full");
+            comboBox2.Items.Add("Poster");
+            comboBox2.Items.Add("Sign Up Sheet");
+            waitlist.Hide();
+            waitlistcount.Hide();
+            attendee.Hide();
+            attendeecount.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  create document
+            try
+            {
+
+                //change date format
+                string date2 = date.Text + "";
+                date.Format = DateTimePickerFormat.Custom;
+                date.CustomFormat = "yyyy.MM.dd";
+
+                //initialize new doc's with naming convention
+                string name = date.Text + " " + what.Text + " with " + who.Text;
+                string filepath = "C:\\Users\\Samuel\\Desktop\\" + name + " " + type + ".docx";
+                Console.WriteLine(type);
+                //  copy poster format to  new doc
+                if (type == "Poster") { 
+                    if (size == "Half")
+                    {
+                        File.Copy("C:\\Users\\Samuel\\Desktop\\HalfTemplate.docx", filepath, true);
+                    }
+                    else {
+                        File.Copy("C:\\Users\\Samuel\\Desktop\\FullTemplate.docx", filepath, true);
+                    }
+                }
+                else
+                {
+                    File.Copy("C:\\Users\\Samuel\\Desktop\\SignUpTemplate.docx", filepath, true);
+                }
+                Console.WriteLine("4");
+                //  create missing object
+                object missing = Missing.Value;
+                //  create Word application object
+                Word.Application wordApp = new Word.Application();
+                //  create Word document object
+                Word.Document aDoc = null;
+                //  create & define filename object with temp.doc
+               
+                object filename = filepath;
+                //  if temp.doc available
+                Console.WriteLine("1");
+                if (File.Exists((string)filename))
+                {
+                    object readOnly = false;
+                    object isVisible = false;
+                    //  make visible Word application
+                    wordApp.Visible = false;
+                    //  open Word document named temp.doc
+                    aDoc = wordApp.Documents.Open(ref filename, ref missing,
+                                                   ref readOnly, ref missing, ref missing, ref missing,
+                                                   ref missing, ref missing, ref missing, ref missing,
+                                                   ref missing, ref isVisible, ref missing, ref missing,
+                                                   ref missing, ref missing);
+                    aDoc.Activate();
+                    //  Call FindAndReplace()function for each change
+                   
+                    this.FindAndReplace(wordApp, "<Event_Name>", what.Text);
+                    this.FindAndReplace(wordApp, "<Host>", who.Text);
+                    this.FindAndReplace(wordApp, "<Date>", date2);
+                    this.FindAndReplace(wordApp, "<Time>", time.Text);
+                    this.FindAndReplace(wordApp, "<Place>", where.Text);
+                    
+                    //set pic dimensions
+                    Word.Range range = aDoc.Bookmarks["picLocation"].Range;
+                    float picHeight;
+                    float picWidth;
+                    float pic1Top;
+                    float pic1Left;
+                    float pic2Top;
+                    float pic2Left;
+
+
+                    if (type == "Poster")
+                    {
+                        if (size == "Half")
+                        {
+                            picHeight = (float)220.07;
+                            picWidth = (float)200.94;
+                            pic1Top = (float)Word.WdShapePosition.wdShapeTop;
+                            pic1Left = (float)Word.WdShapePosition.wdShapeRight;
+                            pic2Top = (float)288;
+                            pic2Left = (float)Word.WdShapePosition.wdShapeRight;
+                        }
+                        else
+                        {
+                            picHeight = (float)247.07;
+                            picWidth = (float)230.94;
+                            pic1Top = (float)147;
+                            pic1Left = (float)Word.WdShapePosition.wdShapeRight;
+                            pic2Top = (float)288;
+                            pic2Left = (float)Word.WdShapePosition.wdShapeRight;
+                        }
+                    }
+                    else
+                    {
+                        picHeight = (float)100.07;
+                        picWidth = (float)120.94;
+                        pic1Top = (float)Word.WdShapePosition.wdShapeTop;
+                        pic1Left = (float)Word.WdShapePosition.wdShapeLeft;
+                        pic2Top = (float)288;
+                        pic2Left = (float)Word.WdShapePosition.wdShapeRight;
+                    }
+                    Console.WriteLine("1");
+                    
+                    //insert top picture
+                    this.insertPicture(aDoc, path, picWidth, picHeight, pic1Top, pic1Left, range);
+                    Console.WriteLine("2");
+                    //insert bottop picture
+                    if ( type != "Sign Up Sheet" && size != "Full")
+                    {
+                        this.insertPicture(aDoc, path, picWidth, picHeight, pic2Top, pic2Left, range);
+                    }
+
+                    //insert table if sign up sheet
+                    if (type == "Sign Up Sheet")
+                    {
+                        object start = 0;
+                        object end = 0;
+                        Word.Range tableLocation = aDoc.Bookmarks["tableLocation"].Range;
+                        aDoc.Tables.Add(tableLocation, 3, 4);
+                    }
+
+                        //save new doc after modified
+                        aDoc.Save();
+
+                    //close doc
+                    wordApp.Quit(Type.Missing, Type.Missing, Type.Missing);
+                }
+                else
+                    MessageBox.Show("File does not exist.",
+            "No File", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in process.", "Internal Error",
+        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+
+            }
+        }
+
+        private void FindAndReplace(Word.Application wordApp,
+            object findText, object replaceText)
+        {
+            
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            wordApp.Selection.Find.Execute(ref findText, ref matchCase,
+                ref matchWholeWord, ref matchWildCards, ref matchSoundsLike,
+                ref matchAllWordForms, ref forward, ref wrap, ref format,
+                ref replaceText, ref replace, ref matchKashida,
+                        ref matchDiacritics,
+                ref matchAlefHamza, ref matchControl);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //store the path
+                path = openFileDialog1.FileName;
+                textBox1.Text = path;
+            }
+        }
+
+        private void insertPicture(Word.Document doc, string path, float width, float height, float top, float left, Word.Range range)
+        {
+            //create picture
+            Word.InlineShape picture = doc.InlineShapes.AddPicture(path, Type.Missing, Type.Missing, range);
+
+            //set height and width 
+            picture.Height = height;
+            picture.Width = width;
+
+            //convert to shape for further customization
+            Word.Shape shape = picture.ConvertToShape();
+            
+            
+            //add a border
+            shape.Line.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
+
+            //send picture behind text then position it
+            shape.WrapFormat.Type = Word.WdWrapType.wdWrapBehind;
+            shape.Left = left;
+            shape.Top = top;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            size = comboBox1.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            type = comboBox2.Text;
+            if (comboBox2.Text == "Sign Up Sheet")
+            {
+                attendee.Show();
+                attendeecount.Show();
+                waitlist.Show();
+                waitlistcount.Show();
+                label6.Hide();
+                comboBox1.Hide();
+            }
+            else {
+                attendee.Hide();
+                attendeecount.Hide();
+                waitlist.Hide();
+                waitlistcount.Hide();
+                label6.Show();
+                comboBox1.Show();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void attendeecount_ValueChanged(object sender, EventArgs e)
+        {
+            attnum = (int)attendeecount.Value;
+        }
+
+        private void waitlistcount_ValueChanged(object sender, EventArgs e)
+        {
+            waitnum = (int)waitlistcount.Value;
+        }
+    }
+
+        
+    }
+
